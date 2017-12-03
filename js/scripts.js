f6ab9619c75df5f82654bad53e8f7179c49ead0e
Margaret Berry <codeberry1@gmail.com>
2017-12-03 08:13:36 -0800

    remove alerts

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,25 @@
 $(document).ready(function(){
   $("#wordplay").submit(function(event) {
     var sentence = $("input#words").val();
-
-    alert(sentence);
-
     var splitWords = sentence.split(" ");
-
     alert(splitWords);
 
     event.preventDefault();
+    
     var newArray = splitWords.map(function(splitWord){
       if(splitWord.length > 3){
         return splitWord;
-      }else{
+      } else {
         return;
       }
     });
-  alert(newArray);
+
     newArray.forEach(function(newA){
       if (!newA) {
-          delete newA;
+        delete newA;
       };
     });
 
-  alert(newArray);
-
-  newArray.join();
-
+    newArray.join();
   });
 });
